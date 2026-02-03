12b9d10c3f131e5239e767be9d9adc63c947d348
dhemsonjanpilapiltubod-creator <dhemsonjanpilapiltubod@gmail.com>
2026-02-03 12:40:19 +0800

    Add files via upload

---
+++ dhemsonport.js
@@ -0,0 +1,12 @@
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    const targetSection = document.getElementById(targetId);
+    window.scrollTo({
+      top: targetSection.offsetTop - 250,
+      behavior: 'smooth'
+    });
+  });
+});
